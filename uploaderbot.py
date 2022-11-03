e47b8d5af32c848ddb373626d63545a17951853a
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2022-11-03 14:01:41 +0000

    Middle block now triggered

--- uploaderbot.py
+++ uploaderbot.py
@@ -7,12 +7,13 @@ import pprint
 logincreds = wdi_login.WDLogin(user="admin", pwd="change-this-password", mediawiki_api_url="http://localhost:80/w/api.php")
 #Item:
 mydata = pd.read_csv("mydata2.csv")
+#mydata = 
 for index, row in mydata.iterrows():
     print("index")
     print(index)
-    for item in row:
-        print("item")
-        print(item)
+    #for item in row:
+        #print("item")
+        #print(item)
     item_statements = []
     current_title=""
     if index==0: #if we've just started, start building from nothing
@@ -23,7 +24,10 @@ for index, row in mydata.iterrows():
                 print("alpha")
                 print(row[i])
                 item_statements.append(wdi_core.WDUrl(row[i], prop_nr="P3"))
-    elif row[0]==None: #if there isn't a new subject
+    #elif row[0]=="Nan": #if there isn't a new subject
+    #elif row[0].empty:
+    #elif row[0]==None:
+    elif pd.isna(row[0]):
         print("row number")
         print(index)
         for i in range (1, len(row)):
