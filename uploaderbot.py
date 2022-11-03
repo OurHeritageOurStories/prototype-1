832d819e4349d538fea9e261df8dbf6da910e984
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2022-11-03 13:28:33 +0000

    further work on uploaderbot

--- uploaderbot.py
+++ uploaderbot.py
@@ -1,5 +1,4 @@
 # load the necessary libraries
-from tokenize import String
 from wikidataintegrator import wdi_core, wdi_login
 import pandas as pd
 import pprint
@@ -45,83 +44,3 @@ for index, row in mydata.iterrows():
                 print("gamma")
                 print(row[i])
                 item_statements.append(wdi_core.WDUrl(row[i], prop_nr="P3"))
-    
-'''    if index!=0:
-
-
-    
-    if index==0:
-        for i in range (1, len(row)):
-            if type(row[i])==str:
-                item_statements.append(wdi_core.WDUrl(item, prop_nr="P3"))
-        wbPage = wdi_core.WDItemEngine(data=item_statements, mediawiki_api_url="http://localhost:80/w/api.php")
-        wbPage.set_label(row[0], lang="en")
-        pprint.pprint(wbPage.get_wd_json_representation())
-        wbPage.write(logincreds)
-        item_statements=[]
-    else:
-        
-
-    print("index")
-    print(index)
-    print("row")
-    print(row)
-    #for item in row:
-    #    print("item")
-    #    print(item)
-    #    print("item tyep")
-    #    print(type(item))
-        #for bit in item:
-        #    print("bit")
-        #    print(bit)
-
-#    if index==0:
-#            item_statements = [] # all statements for one item
-#            for i in range(1, len(row)):
-#                item = row[i]
-#                if type(item)==str:
-#                    item_statements.append(wdi_core.WDUrl(item, prop_nr="P3"))
-#            wbPage = wdi_core.WDItemEngine(data=item_statements, mediawiki_api_url="http://localhost:80/w/api.php")
-#            wbPage.set_label(row[0], lang="en")
-#            pprint.pprint(wbPage.get_wd_json_representation())
-#            wbPage.write(logincreds)
-#    else:
-#        item_statements = [] # all statements for one item
-#        if row[0]==None:
-#            print("dave")
-#            print("index")
-#            print(row)
-#            print("not index")
-#            #for item in row:
-#            for i in range(1, len(row)):
-#                item = row[i]
-#                #if item != None:
-#                if type(item)==str:
-#                    #print(type(item))
-#                    #print(item)
-#                    item_statements.append(wdi_core.WDUrl(item, prop_nr="P3"))
-#                    #item_statements.append(set_label(item, lang="en"))
-#                    #wbPage.set_description(item, lang="en")
-#            print("dave")
-#            ## Prepare the statements to be added
-#            #item_statements = [] # all statements for one item
-#            #item_statements.append(wdi_core.WDString("column1111_value", prop_nr="P1")) 
-#            #item_statements.append(wdi_core.WDItemID("Q42", prop_nr="P2"))
-#            #item_statements.append(wdi_core.WDUrl("http://www.hhha.com", prop_nr="P3"))
-#        else:
-#            ## instantiate the Wikibase page, add statements, labels and descriptions
-#            wbPage = wdi_core.WDItemEngine(data=item_statements, mediawiki_api_url="http://localhost:80/w/api.php")
-#            wbPage.set_label(row[0], lang="en")
-#            #wbPage.set_label("label", lang="en")
-#            #wbPage.set_label("naam", lang="nl")
-#            #wbPage.set_label("nom", lang="fr")
-#            #wbPage.set_description("description", lang="en")
-#            #wbPage.set_description("beschrijving", lang="nl")
-#            #wbPage.set_description("description", lang="fr")#
-
-            ## sanity check (debug)
-#            pprint.pprint(wbPage.get_wd_json_representation())
-
-            ## write data to wikibase
-#            wbPage.write(logincreds)
-#            item_statements = []'''
