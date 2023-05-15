99a93383c490ecf181bae52d2b45272aa882764e
Harshad Gupta <harshad.gupta@nationalarchives.gov.uk>
2023-05-15 15:55:07 +0100

    Template and redercting for 404 page not found

--- neutrinoapp/src/App.jsx
+++ neutrinoapp/src/App.jsx
@@ -7,11 +7,16 @@ import {
 import Home from './Home'
 import Search from './Search'
 import Result from './Result'
+import Page_404 from './Page_404'
 
 export default function App () {
   return (
     <Router basename='/'>
       <Routes>
+        <Route
+          path='*'
+          element={<Page_404 />}
+        />
         <Route
           path='/'
           element={<Home />}
